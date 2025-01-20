326538dc54bb5b4e1c7c5f096cabc210b1d6757c
Mrinmay Das <mrinmaydas2604@gmail.com>
2025-01-20 15:57:34 +0530

    Update server.py

--- text_emotion/ano/server.py
+++ text_emotion/ano/server.py
@@ -2,22 +2,22 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib  # or any library to load your ML model
 
+
+pipe_lr = joblib.load(open("textemotion.pkl", "rb"))
+
 app = Flask(__name__)
 CORS(app)  # To allow cross-origin requests from Flutter
 
-# Load your trained ML model
-model = joblib.load('textemotion.pkl')  # Replace with your model file
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET','POST'])
 def predict():
     data = request.json  # Get JSON data from the request
     user_input = data.get('input')  # Extract the input from JSON
     
     # Process the input and make a prediction
-    prediction = model.predict([user_input])  # Adapt as per your model's input structure
-    
+    results = pipe_lr.predict([user_input])
 
-    return jsonify({'prediction': prediction[0]})  # Send back the prediction
+    return jsonify({'prediction': results[0]})  
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
